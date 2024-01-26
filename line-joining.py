99ae0bfe67269869c5bf03f00549ffc6c6c71aa9
Krish Baryashah <krishbaiasha@gmail.com>
2024-01-27 02:00:56 +0500

    added line-joining.py to repository

---
+++ line-joining.py
@@ -0,0 +1,33 @@
+# Line Joining
+
+#types of line joining
+
+''' 1 Implicit line joing is a way of writing code 
+so it can be understandable easily'''
+
+tuple = ('hello'
+         'bhai'
+         'log'
+         'kia'
+         'hal'
+         'hain'
+         'ap'
+         'sabke')
+
+list = ['apple','banana','Guava','Kiwi','papaya',
+        'Coconut','Mango','Orange','Cherry','Dates'
+        ,'Pomegranate','Mudapples','Sapodilla',
+        'Noseberry']
+
+
+# 2 Explicit
+
+Sentence = 'So this is going to be the very long\
+    sentence in the visual code sorry not \
+    a code but a string cause we can write in it \
+    more comfortable cause we follow a trick of back\
+    slash in it.'
+    
+print(tuple)
+print(list)
+print(Sentence)
