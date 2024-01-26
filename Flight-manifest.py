2ee708d3c7b105f47087c9d540f5a3cd1a587a56
Krish Baryashah <krishbaiasha@gmail.com>
2024-01-27 00:09:16 +0500

    Added Flight-manifest.py in repository

--- Flight-manifest.py
+++ Flight-manifest.py
@@ -6,3 +6,9 @@ manifest = 'Flight: BA234 | Destination: Singapore | Passengers: 400'
 #Indexing
 print(manifest[8:14])
 print(manifest[0:6])
+print(manifest[14:40]) 
+#Indexing me aesa hota hai
+# ke agr 14 se 40 tk jo words show krta hai wo 39 tk hoty hain 
+# 40 wla show nhi krta yani last ka number/words
+
+#concatenation
